f5e603f7ec6a2671ff67fea55fe2d89bf0f18340
Vhelaa1 <halabouzidi@icloud.com>
2023-05-23 00:40:36 +0100

    main.h updated

--- main.h
+++ main.h
@@ -11,5 +11,10 @@
 
 extern char **environ;
 
+int command_exists(char **args);
+int execute_command(char **args, char *userInput_buf);
+int handle_builtins(char **args);
+char **tokenize_input(char *userInput_buf);
+int check_spaces_tabs(char *str);
 
 #endif /* MAIN_H */
