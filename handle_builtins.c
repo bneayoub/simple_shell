668b690bfe0c69b33ed7e70a312993a185d6b507
Vhelaa1 <halabouzidi@icloud.com>
2023-05-23 00:41:54 +0100

    handle bultins

---
+++ handle_builtins.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * handle_builtins - handles the built-in functions
+ * @args: arguments array
+ *
+ * Return: exit success for exit when successful, and 1 or 0 for env
+*/
+
+int handle_builtins(char **args)
+{
+	char **env;
+	if (strcmp(args[0], "exit") == 0)
+	{
+		exit(EXIT_SUCCESS);
+	}
+	else if (strcmp(args[0], "env") == 0)
+	{
+		for (env = environ; *env; ++env)
+			printf("%s\n", *env);
+		return (1);
+	}
+	return (0);
+}
