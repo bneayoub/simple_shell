e0292fa88ae9e89ae390630d19d2632132229bb8
bneayoub <bne.ayoub@gmail.com>
2023-05-23 00:42:43 +0100

    update main.c

--- main.c
+++ main.c
@@ -8,25 +8,17 @@
 int main(void)
 {
 	char *userInput_buf = NULL;
-	size_t size_buf = 0;
+	pid_t pid;
+	size_t buf_size = 0;
 	ssize_t readNO;
 	char **args = NULL;
-	pid_t pid;
-	int status = 0;
-	char *tok;
-	int i = 1;
-	char *path;
-	char *dir;
-	char path_search[4096];
+	int status;
 	int is_interactive = isatty(STDIN_FILENO);
-	int command_exists;
-	char *temp_buf;
-	char *command;
 
 	while (666)
 	{
-		write(1, "$ ", 2);
-		readNO = getline(&userInput_buf, &size_buf, stdin);
+		write(STDOUT_FILENO, "$ ", 2);
+		readNO = getline(&userInput_buf, &buf_size, stdin);
 		if (readNO == -1)
 		{
 			if (!is_interactive)
@@ -38,130 +30,41 @@ int main(void)
 			userInput_buf[readNO - 1] = '\0';
 		if (userInput_buf[0] == '\0')
 			continue;
+		args = tokenize_input(userInput_buf);
 
-		if (strcmp(userInput_buf, "exit") == 0)
+		if (check_spaces_tabs(userInput_buf))
 		{
-			free(userInput_buf);
-			if (args)
-			{
-				for (i = 0; args[i]; i++)
-					free(args[i]);
-				free(args);
-			}
-			exit(EXIT_SUCCESS);
-		}
-
-		if (strcmp(userInput_buf, "env") == 0)
-		{
-			int i;
-			for (i = 0; environ[i]; i++)
-				printf("%s\n", environ[i]);
-			continue;
-		}
-		
-		command_exists = 0;
-		temp_buf = strdup(userInput_buf);
-		command = strtok(temp_buf, " \t\n");
-		if (command[0] == '/')
-		{
-			if (access(command, X_OK) == 0)
-			{
-				command_exists = 1;
-			}
-		}
-		else
-		{
-			path = getenv("PATH");
-			sprintf(path_search, "%s:/usr/bin", path);
-			dir = strtok(path_search, ":");
-			while (dir)
-			{
-				char command_path[4096];
-				snprintf(command_path, sizeof(command_path), "%s/%s", dir, command);
-				if (access(command_path, X_OK) == 0)
-				{
-					command_exists = 1;
-					break;
-				}
-				dir = strtok(NULL, ":");
-			}
-		}
-
-		free(temp_buf);
-
-		if (!command_exists)
-		{
-			char error_message[100];
-			sprintf(error_message, "%s: command not found", command);
-			errno = ENOENT;
-			perror(error_message);
 			continue;
 		}
 
+		if (handle_builtins(args))
+		{
+			free(args);
+			continue;
+		}
+		if (!command_exists(args))
+		{
+			fprintf(stderr, "Command not found: %s\n", args[0]);
+			free(args);
+			continue;
+		}
 		pid = fork();
 		if (pid == -1)
 		{
 			perror("fork");
-			free(userInput_buf);
 			exit(EXIT_FAILURE);
 		}
 		if (pid == 0)
 		{
-			tok = strtok(userInput_buf, " \t\n\"");
-			while (tok)
+			if (execute_command(args, userInput_buf) == -1)
 			{
-				args = realloc(args, (i + 1) * sizeof(char *));
-				args[i - 1] = malloc(sizeof(char) * strlen(tok) + 1);
-				strcpy(args[i - 1], tok);
-				i++;
-				tok = strtok(NULL, " \t\n\"");
+				perror(userInput_buf);
 			}
-			args[i - 1] = NULL;
-			free(tok);
-
-			if (args[0][0] == '/')
-			{
-				if (execve(args[0], args, NULL) == -1)
-				{
-					perror(userInput_buf);
-					free(userInput_buf);
-					exit(EXIT_FAILURE);
-				}
-			}
-			else
-			{
-				char *path = getenv("PATH");
-				if (path == NULL)
-				{
-					write(2, "PATH variable not found.\n", 25);
-					exit(EXIT_FAILURE);
-				}
-
-				sprintf(path_search, "%s:/usr/bin", path);
-
-				dir = strtok(path_search, ":");
-				while (dir)
-				{
-					char command_path[4096];
-					snprintf(command_path, sizeof(command_path), "%s/%s", dir, args[0]);
-					if (access(command_path, X_OK) == 0)
-					{
-						if (execve(command_path, args, NULL) == -1)
-						{
-							perror(userInput_buf);
-							free(userInput_buf);
-							exit(EXIT_FAILURE);
-						}
-					}
-					dir = strtok(NULL, ":");
-				}
-			}
+			exit(EXIT_FAILURE);
 		}
-		else
-		{
-			if (wait(&status) == -1)
-				perror("wait");
-		}
+		if (wait(&status) == -1)
+			perror("wait");
+		free(args);
 		if (!is_interactive)
 			break;
 	}
