2e79af3538d41381b286dde425b47de81149f50d
bneayoub <bne.ayoub@gmail.com>
2023-05-23 09:43:47 +0100

    double free solved

--- validate_command.c
+++ validate_command.c
@@ -9,13 +9,12 @@ int validate_command(char **args)
 {
 	if (handle_builtins(args))
 	{
-		free(args);
 		return (0);
 	}
 	if (!command_exists(args))
 	{
 		fprintf(stderr, "Command not found: %s\n", args[0]);
-		free(args);
+		free_args(args);
 		return (0);
 	}
 
