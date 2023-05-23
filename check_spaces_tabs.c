b9baf9fc0a4b20817f9210f3b4bb3144db290f85
bneayoub <bne.ayoub@gmail.com>
2023-05-23 01:34:21 +0100

    check spaces and tabs betty

--- check_spaces_tabs.c
+++ check_spaces_tabs.c
@@ -1,10 +1,15 @@
 #include "main.h"
-
+/**
+ * check_spaces_tabs - checks if a string is only spaces and tabs
+ * @str: user input buffer
+ *
+ * Return: 0 or 1
+*/
 int check_spaces_tabs(char *str)
 {
-	while(*str)
+	while (*str)
 	{
-		if (*str != ' ' && *str !='\t')
+		if (*str != ' ' && *str != '\t')
 			return (0);
 		str++;
 	}
