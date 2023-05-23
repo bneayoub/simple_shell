dcbc76d315ce67eaf10b641f1b45a14ddd746c00
bneayoub <bne.ayoub@gmail.com>
2023-05-23 09:43:25 +0100

    tokenize function updated to successfully allocate

--- tokenize_input.c
+++ tokenize_input.c
@@ -7,21 +7,22 @@
  */
 char **tokenize_input(char *userInput_buf)
 {
-    char **args = NULL;
-    char *separator = " \n\t";
-    char *parsed;
-    int index = 0;
+	char **args = NULL;
+	char *separator = " \n\t";
+	char *parsed;
+	int index = 0;
 
-    parsed = strtok(userInput_buf, separator);
+	parsed = strtok(userInput_buf, separator);
 
-    while (parsed != NULL)
-    {
-        args = realloc(args, (index + 2) * sizeof(char *));
-        args[index] = malloc(strlen(parsed) + 1);
-        strcpy(args[index++], parsed);
-        parsed = strtok(NULL, separator);
-    }
+	while (parsed != NULL)
+	{
+		args = realloc(args, (index + 2) * sizeof(char *));
+		args[index] = malloc(strlen(parsed) + 1);
+		strcpy(args[index++], parsed);
+		parsed = strtok(NULL, separator);
+	}
 
-    args[index] = NULL;
-    return args;
+	args[index] = NULL;
+	free(parsed);
+	return (args);
 }
