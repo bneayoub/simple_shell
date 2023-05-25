bef35fb8d2b37f409bc298c000fa2309b573d01b
bneayoub <bne.ayoub@gmail.com>
2023-05-25 02:41:50 +0100

    debugging

--- command_exists.c
+++ command_exists.c
@@ -24,7 +24,7 @@ int command_exists(char **args)
 			exit(EXIT_FAILURE);
 		}
 
-		sprintf(path_search, "%s:/usr/bin", path);
+		_sprintf(path_search, "%s:/usr/bin", path);
 
 		dir = strtok(path_search, ":");
 		while (dir)
